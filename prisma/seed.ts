c7afe7d2734c8776080b99b39b6c09956c313f5e
Hadrian Chio <hadrian.chio@gmail.com>
2023-03-07 16:18:42 +0100

    adjusted for planetscale and mysql connection

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,28 +5,62 @@ const prisma = new PrismaClient();
 // user1 and user2 testing
 async function main() {
   const user1 = await prisma.user.upsert({
-    where: { email: 'hadrian.chio@gmail.com' },
+    where: { email: 'max.muster@gmail.com' },
     update: {},
     create: {
-      email: 'hadrian.chio@gmail.gom',
-      username: 'Hadrian',
-      password: '123456',
+      email: 'max.muster@gmail.com',
+      username: 'Max',
+      password: '*************************************',
       createdAt: new Date(),
     },
   });
 
-  const user2 = await prisma.user.upsert({
-    where: { email: 'sarah.candolfi@gmail.com' },
+  const goal1 = await prisma.goal.upsert({
+    where: { id: 1 },
     update: {},
     create: {
-      email: 'sarah.candolfi@gmail.gom',
-      username: 'Sarah',
-      password: '123456',
+      userId: user1.id,
+      title: 'setting up PlanetScale',
+      imageUrl: '',
+      description:
+        'Setting up planetscale and prisma to make them work together',
+      importance: 'It is important to have a database',
+      vision: 'I want to have a database',
       createdAt: new Date(),
+      updatedAt: new Date(),
     },
   });
 
-  console.log({ user1, user2 });
+  const journal1 = await prisma.journal.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      userId: user1.id,
+      title: 'First Journal',
+      mood: 'happy',
+      moodDescription: 'I am happy',
+      activity: 'I am doing mindfulness staff',
+      toImprove: 'I want to improve my mood',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+
+  const task1 = await prisma.task.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      userId: user1.id,
+      title: 'First Task',
+      description: 'First Task Description',
+      plannedDate: new Date(),
+      completed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+
+  console.log({ user1, goal1, journal1, task1 });
 }
 
 main()
